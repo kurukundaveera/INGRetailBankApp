2052e55d2fc108ba6545f17642b45a557316950a
udaych31 <hariharamohanty@gmail.com>
2019-06-11 17:05:57 +0530

    changes

--- src/main/java/com/hcl/ing/retailbank/app/dto/LoginResponse.java
+++ src/main/java/com/hcl/ing/retailbank/app/dto/LoginResponse.java
@@ -4,17 +4,19 @@ import java.io.Serializable;
 
 import com.hcl.ing.retailbank.app.util.ApiResponse;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
-@Data
-@Setter
-@Getter
 public class LoginResponse  extends  ApiResponse  implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
 	private Long accounNumber;
+	
+	public Long getAccounNumber() {
+		return accounNumber;
+	}
+	public void setAccounNumber(Long accounNumber) {
+		this.accounNumber = accounNumber;
+	}
+	
+	
 
 }
--- src/main/java/com/hcl/ing/retailbank/app/entity/User.java
+++ src/main/java/com/hcl/ing/retailbank/app/entity/User.java
@@ -9,30 +9,54 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 @Entity
-@Table(name="USER_INFO")
-@Data @Getter @Setter @ToString
+@Table(name = "USER_INFO")
+
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
-	
+
 	@Column
 	private String userName;
-	
+
 	@Column
 	private String password;
-	
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public User() {
 		super();
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", password=" + password + "]";
 	}
 
 }
