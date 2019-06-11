412f3b6c6e5f4204b3575a6bd7b5cfcb9b4c1433
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-06-11 13:30:50 +0530

    changes

--- src/main/java/com/hcl/ing/retailbank/app/service/RetailBankService.java
+++ src/main/java/com/hcl/ing/retailbank/app/service/RetailBankService.java
@@ -8,6 +8,11 @@ import com.hcl.ing.retailbank.app.dto.AccountSummaryResponse;
 
 public interface RetailBankService {
 	
+	/**
+	 * This API  For TRANSFER FUNDS
+	 * @param request
+	 * @return
+	 */
 	public ResponseEntity<FundTransferResponse> fundTransfer(FundTransferRequest request);
 
 	ResponseEntity<AccountSummaryResponse> accountSummary(Long acccountNumber);
